2179734ae237ba0ddae1634fb1ce490057a94db2
RedFoxFinn <laborantti.antti@gmail.com>
2021-01-20 22:11:03 +0200

    started ruuvipenkki

---
+++ ruuvipenkki.js
@@ -0,0 +1,24 @@
+
+const express = require('express'),
+  ruuvipenkki = express();
+const http = require('http');
+const path = require('path');
+const parser = require('body-parser');
+
+ruuvipenkki.use(parser.json());
+ruuvipenkki.use(parser.urlencoded({extended=false}));
+
+ruuvipenkki.route('/')
+  .get((req, res) => {
+    res.json({"ruuvipenkki": "whoo!"});
+  });
+  /*
+  .get((req, res) => {
+    ruuvipenkki.use(express.static('build'));
+    res.sendFile(path.join(__dirname, '/build/index.html'));
+  });
+  */
+
+ruuvipenkki.listen(port = 4004, () => {
+  console.log(`Ruuvipenkki is now running.`)
+});
